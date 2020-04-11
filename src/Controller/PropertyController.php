07ac6ebfa68d319903d24ca1af5a364275eb962d
Anas TULIMAT <anas.tulimat@edu.esiee.fr>
2020-04-11 20:20:50 +0200

    feat: add serach and filter for property

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 
 
 use App\Entity\Property;
+use App\Entity\PropertySearch;
+use App\Form\PropertySearchType;
 use App\Repository\PropertyRepository;
 use Doctrine\Common\Persistence\ObjectManager;
 use Knp\Component\Pager\PaginatorInterface;
@@ -47,14 +49,19 @@ class PropertyController extends AbstractController
      */
     public function index(PaginatorInterface $paginator, Request $request): Response
     {
+        $search = new PropertySearch();
+        $from = $this->createForm(PropertySearchType::class, $search);
+        $from->handleRequest($request);
+
         $properties = $paginator->paginate(
-            $this->repository->findAllVisibleQuery(),
+            $this->repository->findAllVisibleQuery($search),
             $request->query->getInt('page', 1),
             12
         );
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties',
-            'properties' => $properties
+            'properties' => $properties,
+            'form' => $from->createView()
         ]);
     }
 
